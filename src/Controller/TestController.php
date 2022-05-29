859ac957701a4459edb7f59dbd069892182640b9
Elodie Sponton <elodie.sponton@yahoo.fr>
2022-05-29 19:02:43 +0200

    Découverte de la notion de container

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -2,18 +2,25 @@
 
 namespace App\Controller;
 
+use App\Taxes\Calculator;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class TestController {
 
+
+
+    /**
+     * @Route("/", name="index")
+     */
     public function index() {
+
         dd("Ca fonctionne");
     }
 
     /**
-     * @Route("/test/{age<\d+>?0}", name="test", methods={"GET", "POST"}, host="localhost", schemes={"https", "http"})
+     * @Route("/test/{age<\d+>?0}", name="test")
      */
 
     public function test(Request $request, $age) {
